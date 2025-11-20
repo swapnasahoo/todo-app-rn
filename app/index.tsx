e33ec763cc19d54fed8487ba3caa01aa65a0f3ea
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2025-11-20 10:38:24 +0530

    Added FlatList to render todos

--- app/index.tsx
+++ app/index.tsx
@@ -2,45 +2,62 @@ import Feather from "@expo/vector-icons/Feather";
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import React, { useState } from "react";
-import { Pressable, ScrollView, Text, TextInput, View } from "react-native";
+import { FlatList, Pressable, Text, TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const index = () => {
-  const [isCompleted, setIsCompleted] = useState(false);
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      value: "Watch youtube",
+      completed: false,
+    },
+    {
+      id: 2,
+      value: "Study",
+      completed: true,
+    },
+  ]);
 
   return (
     <View className="p-4 bg-gray-900 flex-1">
-      <SafeAreaView>
-        <ScrollView>
-          <View>
-            {/* TODO HEADER */}
-            <Text
-              className="text-amber-500 text-4xl font-semibold m-4 mt-6
+      <SafeAreaView
+        style={{
+          flex: 1,
+        }}
+      >
+        {/* TODO HEADER */}
+        <Text
+          className="text-amber-500 text-4xl font-semibold m-4 mt-6
           "
-            >
-              TodoApp
-            </Text>
-            {/* TODO INPUT */}
-            <View className="flex-1 justify-center mx-4 mt-8 relative ">
-              <TextInput
-                className="w-full h-14 bg-transparent border-2 border-blue-100 rounded-full text-xl p-4 py-0 text-white placeholder:text-2x placeholder:text-[#b5b7b8]"
-                placeholder="Enter todo name"
-              ></TextInput>
-              <Pressable
-                style={{
-                  position: "absolute",
-                  right: 20,
-                }}
-                onPress={() => console.log(`hello i am pressed ${1}`)}
-              >
-                <FontAwesome5 name="plus-circle" size={24} color="#fbbf24" />
-              </Pressable>
-            </View>
+        >
+          TodoApp
+        </Text>
+        {/* TODO INPUT */}
+        <View className="justify-center mx-4 mt-8 relative ">
+          <TextInput
+            className="w-full h-14 bg-transparent border-2 border-blue-100 rounded-full text-xl p-4 py-0 text-white placeholder:text-2x placeholder:text-[#b5b7b8]"
+            placeholder="Enter todo name"
+          ></TextInput>
+          <Pressable
+            style={{
+              position: "absolute",
+              right: 20,
+            }}
+            onPress={() => console.log(`hello i am pressed ${1}`)}
+          >
+            <FontAwesome5 name="plus-circle" size={24} color="#fbbf24" />
+          </Pressable>
+        </View>
 
-            {/* TODO LIST CARD */}
-            <View className="mt-8 gap-4">
-              <View className="flex-row items-start bg-gray-800 gap-4 p-4 rounded-xl shadow-xs">
-                {isCompleted ? (
+        {/* TODO LIST CARD */}
+        <FlatList
+          data={todos}
+          keyExtractor={(todo) => todo.id.toString()}
+          renderItem={({ item }) => {
+            return (
+              <View className="flex-row items-start bg-gray-800 gap-4 p-4 rounded-xl shadow-xs mb-4">
+                {item.completed ? (
                   <Feather name="check-circle" size={24} color="white" />
                 ) : (
                   <Feather name="circle" size={24} color="white" />
@@ -48,9 +65,9 @@ const index = () => {
                 <View>
                   <Text
                     className={`font-semibold text-2xl 
-                              ${isCompleted ? "line-through text-gray-500" : "text-white"}`}
+                              ${item.completed ? "line-through text-gray-500" : "text-white"}`}
                   >
-                    Watch Youtube
+                    {item.value}
                   </Text>
                   <View className="flex-row gap-4 ml-4 mt-2">
                     <FontAwesome6 name="edit" size={24} color="white" />
@@ -62,32 +79,16 @@ const index = () => {
                   </View>
                 </View>
               </View>
-              <View className="flex-row items-start bg-gray-800 gap-4 p-4 rounded-xl shadow-xs">
-                {isCompleted ? (
-                  <Feather name="check-circle" size={24} color="white" />
-                ) : (
-                  <Feather name="circle" size={24} color="white" />
-                )}
-                <View>
-                  <Text
-                    className={`font-semibold text-2xl 
-                              ${isCompleted ? "line-through text-gray-500" : "text-white"}`}
-                  >
-                    Watch Youtube
-                  </Text>
-                  <View className="flex-row gap-4 ml-4 mt-2">
-                    <FontAwesome6 name="edit" size={24} color="white" />
-                    <FontAwesome6
-                      name="trash-arrow-up"
-                      size={24}
-                      color="white"
-                    />
-                  </View>
-                </View>
-              </View>
-            </View>
-          </View>
-        </ScrollView>
+            );
+          }}
+          style={{
+            marginTop: 48,
+          }}
+          contentContainerStyle={{
+            paddingBottom: 48,
+          }}
+          showsVerticalScrollIndicator={false}
+        />
       </SafeAreaView>
     </View>
   );
