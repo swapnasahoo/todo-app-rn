fd25d086aa5030bc124e72fbbc52c233580ec30c
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2025-11-20 21:19:11 +0530

    Added color scheme from react native to automatically set by system theme

--- app/index.tsx
+++ app/index.tsx
@@ -3,10 +3,19 @@ import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import React, { useEffect, useState } from "react";
-import { FlatList, Pressable, Text, TextInput, View } from "react-native";
+import {
+  FlatList,
+  Pressable,
+  Text,
+  TextInput,
+  useColorScheme,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const index = () => {
+  const scheme = useColorScheme();
+
   const [todos, setTodos] = useState<
     { id: Number; value: String; completed: Boolean }[]
   >([]);
@@ -49,7 +58,7 @@ const index = () => {
   }
 
   return (
-    <View className="p-4 bg-gray-900 flex-1">
+    <View className="p-4 bg-amber-50 dark:bg-gray-900 flex-1">
       <SafeAreaView
         style={{
           flex: 1,
@@ -57,7 +66,7 @@ const index = () => {
       >
         {/* TODO HEADER */}
         <Text
-          className="text-amber-500 text-4xl font-semibold m-4 mt-6
+          className="text-amber-600 dark:text-amber-500 text-4xl font-semibold m-4 mt-6
           "
         >
           TodoApp
@@ -65,7 +74,7 @@ const index = () => {
         {/* TODO INPUT */}
         <View className="justify-center mx-4 mt-8 relative ">
           <TextInput
-            className="w-full h-14 bg-transparent border-2 border-blue-100 rounded-full text-xl p-4 py-0 text-white placeholder:text-2x placeholder:text-[#b5b7b8]"
+            className="w-full h-14 bg-transparent border-2 border-blue-700 dark:border-blue-100 rounded-full text-xl p-4 py-0 text-black dark:text-white placeholder:text-2x placeholder:text-[#2a2a2a] placeholder:dark:text-[#b5b7b8]"
             placeholder="Enter todo name"
             onChangeText={setTodoValue}
             value={todoValue}
@@ -77,7 +86,11 @@ const index = () => {
             }}
             onPress={addTodo}
           >
-            <FontAwesome5 name="plus-circle" size={24} color="#fbbf24" />
+            <FontAwesome5
+              name="plus-circle"
+              size={24}
+              color={scheme === "light" ? "#2a2a2a" : "#fbbf24"}
+            />
           </Pressable>
         </View>
 
@@ -87,27 +100,39 @@ const index = () => {
           keyExtractor={(todo) => todo.id.toString()}
           renderItem={({ item }) => {
             return (
-              <View className="flex-row items-start bg-gray-800 gap-4 p-4 rounded-xl shadow-xs mb-4">
+              <View className="flex-row items-start bg-gray-100 dark:bg-gray-800 gap-4 p-4 rounded-xl shadow-xs mb-4">
                 <Pressable onPress={() => completeTodo(item.id)}>
                   {item.completed ? (
-                    <Feather name="check-circle" size={24} color="white" />
+                    <Feather
+                      name="check-circle"
+                      size={24}
+                      color={scheme === "light" ? "black" : "white"}
+                    />
                   ) : (
-                    <Feather name="circle" size={24} color="white" />
+                    <Feather
+                      name="circle"
+                      size={24}
+                      color={scheme === "light" ? "black" : "white"}
+                    />
                   )}
                 </Pressable>
                 <View>
                   <Text
                     className={`font-semibold text-2xl 
-                              ${item.completed ? "line-through text-gray-500" : "text-white"}`}
+                              ${item.completed ? "line-through text-gray-500" : "text-black dark:text-white"}`}
                   >
                     {item.value}
                   </Text>
                   <View className="flex-row gap-4 ml-4 mt-2">
-                    <FontAwesome6 name="edit" size={24} color="white" />
+                    <FontAwesome6
+                      name="edit"
+                      size={24}
+                      color={scheme === "light" ? "black" : "white"}
+                    />
                     <FontAwesome6
                       name="trash-arrow-up"
                       size={24}
-                      color="white"
+                      color={scheme === "light" ? "black" : "white"}
                       onPress={() => deleteTodo(item.id)}
                     />
                   </View>
